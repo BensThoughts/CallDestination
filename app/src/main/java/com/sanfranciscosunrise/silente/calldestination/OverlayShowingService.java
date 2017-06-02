64a5c668cd2b69ad865a97edb69ceae7e85d15a7
ben <SanFranciscoSunrise@GMail.com>
2017-06-02 16:42:39 -0700

    Commit 0.0.0.12: renamed revealCancelButton() to createCancelButton and hideCancelButton to removeCancelButton
    removed the implements View.OnClickListener and proviced each button it's on version of View.OnClick

--- app/src/main/java/com/sanfranciscosunrise/silente/calldestination/OverlayShowingService.java
+++ app/src/main/java/com/sanfranciscosunrise/silente/calldestination/OverlayShowingService.java
@@ -7,6 +7,7 @@ import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.net.Uri;
 import android.os.Binder;
+import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.Gravity;
@@ -33,7 +34,7 @@ import android.widget.ImageButton;
  *    the BAAS (Buttons as a service)? That might be bad, how do I fix this?
  */
 
-public class OverlayShowingService extends Service implements View.OnTouchListener, View.OnClickListener {
+public class OverlayShowingService extends Service implements View.OnTouchListener {
     private static final String TAG = "OverlayShowingService";
 
     private final IBinder mBinder = new LocalBinder();
@@ -88,6 +89,33 @@ public class OverlayShowingService extends Service implements View.OnTouchListen
         overlayButton.setMaxHeight(2);
         overlayButton.setMaxWidth(2);
 
+        overlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (modeSearchOrCall) {
+                    Intent searchIntent = new Intent(getApplicationContext(), PlacePickerActivity.class);
+                    //searchIntent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                    searchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(searchIntent);
+                } else {
+                    if (!mClicked) {
+                        createCancelButton();
+                        mClicked = true;
+                    } else {
+                        if (mDestinationPhoneNumber != null) {
+                            Intent phoneCallIntent = new Intent(Intent.ACTION_DIAL,
+                                    Uri.fromParts("tel", mDestinationPhoneNumber, null));
+                            phoneCallIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(phoneCallIntent);
+                        }
+                        removeCancelButton();
+                        mClicked = false;
+                    }
+                }
+            }
+        });
+
+
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
@@ -98,45 +126,7 @@ public class OverlayShowingService extends Service implements View.OnTouchListen
         params.x = 0;
         params.y = 0;
         wm.addView(overlayButton, params);
-
-        /***************************************************************
-         *
-         * Not sure if we are going to keep this cancel button,
-         * looking for less resource intensive ways of doing this first
-         * seems two buttons are not greater then one, as the app gets shutdown
-         * by the system with two for I'm guessing resource reasons
-         *
-         * still need to profile this
-         * and look at alternatives.
-         *
-        cancelButton = new ImageButton(this);
-        cancelButton.setOnTouchListener(this);
-        cancelButton.setBackgroundColor(0x55fe4444);
-        cancelButton.setOnClickListener(this);
-        cancelButton.setAlpha(0.7f);
-        cancelButton.setCropToPadding(true);
-        cancelButton.setHovered(true);
-        //cancelButton.setMaxHeight(2);
-        //cancelButton.setMaxWidth(2);
-        cancelButton.setImageResource(R.mipmap.ic_cancel);
-
-        WindowManager.LayoutParams cancelParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
-                PixelFormat.TRANSLUCENT);
-
-        cancelParams.gravity = Gravity.LEFT | Gravity.TOP;
-        cancelParams.width = 40;
-        cancelParams.height = 40;
-        cancelParams.x = params.x + 70;
-        cancelParams.y = 0;
-        Log.i(TAG, "cancelButton.getHeight() == " + cancelButton.getHeight());
-        wm.addView(cancelButton, cancelParams);
-        Log.i(TAG, "cancelButton.getHeight() == " + cancelButton.getHeight());
-         ***************************************************************/
-
-
+        
         topLeftView = new View(this);
         //WindowManager.LayoutParams topLeftParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT,
         //        WindowManager.LayoutParams.WRAP_CONTENT,
@@ -160,10 +150,7 @@ public class OverlayShowingService extends Service implements View.OnTouchListen
             overlayButton = null;
             topLeftView = null;
         }
-        if (cancelButton !=null) {
-            wm.removeView(cancelButton);
-            cancelButton = null;
-        }
+        removeCancelButton();
     }
 
     @Override
@@ -217,37 +204,6 @@ public class OverlayShowingService extends Service implements View.OnTouchListen
         return false;
     }
 
-    @Override
-    public void onClick(View v) {
-        Log.i(TAG, "" + v.getId());
-        if (v.getId() == overlayButton.getId()) {
-            if (modeSearchOrCall) {
-                Intent searchIntent = new Intent(this, PlacePickerActivity.class);
-                //searchIntent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-                searchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(searchIntent);
-            } else {
-                if (!mClicked) {
-                    revealCancelButton();
-                    mClicked = true;
-                } else {
-                    if (mDestinationPhoneNumber != null) {
-                        Intent phoneCallIntent = new Intent(Intent.ACTION_DIAL,
-                                Uri.fromParts("tel", mDestinationPhoneNumber, null));
-                        phoneCallIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(phoneCallIntent);
-                    }
-                    hideCancelButton();
-                    mClicked = false;
-                }
-            }
-        } else if (mClicked) {
-            Log.i(TAG, "Inside cancelButton click");
-            setModeSearch(true);
-            hideCancelButton();
-        }
-    }
-
     public void setDestinationPhoneNumber(String destinationPhoneNumber) {
         mDestinationPhoneNumber = destinationPhoneNumber;
         QueryPreferences.setPrefLastKnownPhoneNumber(this, mDestinationPhoneNumber);
@@ -269,32 +225,45 @@ public class OverlayShowingService extends Service implements View.OnTouchListen
         }
     }
 
-    public void revealCancelButton() {
+    public void createCancelButton() {
         cancelButton = new ImageButton(this);
         cancelButton.setOnTouchListener(this);
         cancelButton.setBackgroundColor(0x55fe4444);
-        cancelButton.setOnClickListener(this);
         cancelButton.setAlpha(0.7f);
         //cancelButton.setCropToPadding(true);
         //cancelButton.setHovered(true);
         cancelButton.setImageResource(R.mipmap.ic_cancel);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mClicked) {
+                    Log.i(TAG, "Inside cancelButton click");
+                    setModeSearch(true);
+                    removeCancelButton();
+                }
+            }
+        });
 
         WindowManager.LayoutParams cancelParams = (WindowManager.LayoutParams)overlayButton.getLayoutParams();
         cancelParams.x =  cancelParams.x + overlayButton.getWidth();
         wm.addView(cancelButton, cancelParams);
+
+        new CountDownTimer(5000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) { }
+
+            @Override
+            public void onFinish() {
+                removeCancelButton();
+            }
+        };
     }
 
-    private void hideCancelButton() {
+    private void removeCancelButton() {
         if (cancelButton !=null) {
+            mClicked = false;
             wm.removeView(cancelButton);
             cancelButton = null;
         }
     }
-
-    private Point getViewCoordinates(View view) {
-        int[] location = new int[2];
-        view.getLocationOnScreen(location);
-        return new Point(location[0], location[1]);
-    }
-
 }
